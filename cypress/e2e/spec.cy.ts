578d41f036f71acd620293ec10070021a0e107c8
Perry Angelora <pea.mail.list@gmail.com>
2024-04-10 10:02:43 -0400

    Updated tests to reflect updated routes

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -65,7 +65,7 @@ describe('Tests for 1200px and above', () => {
   it('should find the "About" link and verify it points to the correct path', () => {
     cy.get('.main_wrap-about_wrap nav')
       .contains('a', 'About')
-      .should('have.attr', 'href', '/about');
+      .should('have.attr', 'href', '/about/');
   });
 
   it('should have a github link', () => {
@@ -100,12 +100,12 @@ describe('Find project links on main page', () => {
   });
   it('should find the nyt pilot project on the main page', () => {
     cy.get('.landing_project-ul li')
-      .find('a[href="/project/the-times-pilot"]')
+      .find('a[href="/project/the-times-pilot/"]')
       .should('exist');
   });
   it('should find the nyt pilot project on the main page', () => {
     cy.get('.landing_project-ul li')
-      .find('a[href="/project/circuit-app"]')
+      .find('a[href="/project/circuit-app/"]')
       .should('exist');
   });
 });
@@ -126,7 +126,7 @@ describe('Tests for NYT Page', () => {
   });
   it('should have a header with nav about button', () => {
     cy.get('.project_navbar-content_wrapper nav')
-      .find('a[href="/about"]')
+      .find('a[href="/about/"]')
       .should('exist');
   });
 
@@ -209,7 +209,7 @@ describe('Tests for Circuit App Page', () => {
   });
   it('should have a header with nav about button', () => {
     cy.get('.project_navbar-content_wrapper nav')
-      .find('a[href="/about"]')
+      .find('a[href="/about/"]')
       .should('exist');
   });
 
@@ -302,7 +302,7 @@ describe('Tests for about page', () => {
   });
   it('should have a header with nav about button', () => {
     cy.get('.project_navbar-content_wrapper nav')
-      .find('a[href="/about"]')
+      .find('a[href="/about/"]')
       .should('exist')
       .should('have.attr', 'class', '--current-page');
   });
@@ -330,5 +330,98 @@ describe('Tests for about page', () => {
     cy.get('.project_footer-link')
       .find('button')
       .should('exist');
+  });
+});
+
+describe('Check click-through of links on home screen', () => {
+  beforeEach(() => {
+    cy.visit(website);
+  });
+
+  it('should finds and clicks the about link - on desktop,', () => {
+    cy.viewport('macbook-13');
+    cy.get('.main_wrap-about_wrap nav')
+      .find('a[href="/about/"]')
+      .click();
+    cy.url().should('contain', '/about/');
+  });
+
+  it('should finds and clicks the times pilot link - on desktop,', () => {
+    cy.viewport('macbook-13');
+    cy.get('.landing_project-ul')
+      .find('a[href="/project/the-times-pilot/"]')
+      .click();
+    cy.url().should('contain', '/project/the-times-pilot/');
+  });
+
+  it('should finds and clicks the times pilot link - on desktop,', () => {
+    cy.viewport('macbook-13');
+    cy.get('.landing_project-ul')
+      .find('a[href="/project/circuit-app/"]')
+      .click();
+    cy.url().should('contain', '/project/circuit-app/');
+  });
+
+  it('should finds and clicks the about link - on small,', () => {
+    cy.viewport('iphone-x');
+    cy.get('.project_footer-home_page-about_link')
+      .find('a[href="/about/"]')
+      .click();
+    cy.url().should('contain', '/about/');
+  });
+
+  it('should finds and clicks the times pilot link - on small,', () => {
+    cy.viewport('iphone-x');
+    cy.get('.landing_project-ul')
+      .find('a[href="/project/the-times-pilot/"]')
+      .click();
+    cy.url().should('contain', '/project/the-times-pilot/');
+  });
+
+  it('should finds and clicks the times pilot link - on small,', () => {
+    cy.viewport('iphone-x');
+    cy.get('.landing_project-ul')
+      .find('a[href="/project/circuit-app/"]')
+      .click();
+    cy.url().should('contain', '/project/circuit-app/');
+  });
+});
+
+describe('Check click-through of links from times-pilot screen', () => {
+  beforeEach(() => {
+    cy.visit(`${website}/project/the-times-pilot/`);
+  });
+
+  it('should link back to the home page', () => {
+    cy.get('.project_navbar nav')
+      .find('a[href="/"]')
+      .click();
+    cy.url().should('equal', `${website}/`);
+  });
+
+  it('should link back to the about page', () => {
+    cy.get('.project_navbar nav')
+      .find('a[href="/about/"]')
+      .click();
+    cy.url().should('contain', '/about/');
+  });
+});
+
+describe('Check click-through of links from circuit-app screen', () => {
+  beforeEach(() => {
+    cy.visit(`${website}/project/circuit-app/`);
+  });
+  it('should link back to the home page', () => {
+    cy.get('.project_navbar nav')
+      .find('a[href="/"]')
+      .click();
+    cy.url().should('equal', `${website}/`);
+  });
+
+  it('should link back to the about page', () => {
+    cy.get('.project_navbar nav')
+      .find('a[href="/about/"]')
+      .click();
+    cy.url().should('contain', '/about/');
   });
 });
